442f28ddb88c833e938eddc1acae1860dba0ab52
jackybek <jackybek@gmail.com>
2023-12-28 18:17:10 +0800

    Update SV_Misc.c
    copy from opcuaserver-33

--- SV_Misc.c
+++ SV_Misc.c
@@ -1,4 +1,7 @@
-#include "open62541.h"
+//#include "open62541.h"
+#include <open62541/plugin/log_stdout.h>
+#include <open62541/server.h>
+#include <open62541/server_config_default.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -6,90 +9,10 @@
 #include <libxml2/libxml/parser.h>
 #include <libxml2/libxml/tree.h>
 #include <libxml2/libxml/xmlreader.h>
-#include "json_checker.h"
-#include "json.h"               //https://raw.githubusercontent.com/udp/json-parser/master/json.h
 
 xmlNodePtr srSeekChildNodeNamed(xmlNode* p, char* name, int len);
 UA_ByteString loadFile(char* filename);
 char* itoa(int num, char* str, int base);
-
-
-// json library
-static void print_depth_shift(int depth);
-static void process_object(json_value *value, int depth);
-static void process_array(json_value* value, int depth);
-static void process_value(json_value* value, int depth);
-
-static void print_depth_shift(int depth)
-{
-        int j;
-        for (j=0; j < depth; j++) {
-                printf(" ");
-        }
-}
-
-static void process_object(json_value* value, int depth)
-{
-        int length, x;
-        if (value == NULL) {
-                return;
-        }
-        length = value->u.object.length;
-        for (x = 0; x < length; x++) {
-                print_depth_shift(depth);
-                printf("object[%d].name = %s\n", x, value->u.object.values[x].name);
-                process_value(value->u.object.values[x].value, depth+1);
-        }
-}
-
-static void process_array(json_value* value, int depth)
-{
-        int length, x;
-        if (value == NULL) {
-                return;
-        }
-        length = value->u.array.length;
-        printf("array\n");
-        for (x = 0; x < length; x++) {
-                process_value(value->u.array.values[x], depth);
-        }
-}
-
-static void process_value(json_value* value, int depth)
-{
-        int j;
-        if (value == NULL) {
-                return;
-        }
-        if (value->type != json_object) {
-                print_depth_shift(depth);
-        }
-        switch (value->type) {
-                case json_none:
-                        printf("none\n");
-                        break;
-                case json_object:
-                        process_object(value, depth+1);
-                        break;
-                case json_array:
-                        process_array(value, depth+1);
-                        break;
-                case json_integer:
-                        printf("int: %10" PRId64 "\n", value->u.integer);
-                        break;
-                case json_double:
-                        printf("double: %f\n", value->u.dbl);
-                        break;
-                case json_string:
-                        printf("string: %s\n", value->u.string.ptr);
-                        break;
-                case json_boolean:
-                        printf("bool: %d\n", value->u.boolean);
-                        break;
-        }
-}
-// end json libary
-
 
 xmlNodePtr srSeekChildNodeNamed(xmlNode* p, char* name, int len)
 {
@@ -182,102 +105,3 @@ char* itoa(int num, char* buffer, int base) {
     buffer[curr] = '\0';
     return buffer;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-#ifdef XXX
-static void
-callback_new(UA_ByteString *encodedBuffer, UA_ByteString *topic)
-{
-
-     int i;
-
-     UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "SV_PubSub.c callback() : Message Received in MQTT Broker : %s ", (char*)topic->data);
-
-     printf("==================================\n");
-     printf("SV_PubSub: Start callback()=======\n");
-
-     // printf("before processing: <%s>\n", (char *)encodedBuffer->data);
-     // remove unwanted characters in reverse order
-     for (i=strlen((char*)encodedBuffer->data); i>0; i--)
-     {
-        //printf("value of i is %d \n", i);
-        if ((char)encodedBuffer->data[i] == '}')
-                break;
-     }
-     encodedBuffer->data[i+1] ='\0';
-
-
-     printf("after processing: \n%s\n", (char *)encodedBuffer->data);
-
-     JSON_checker jc = new_JSON_checker(20);    // depth of tree = 20
-     if (jc)
-     {
-        for (i=0; i < strlen((char*)encodedBuffer->data); i++)
-        {
-                int next_char = encodedBuffer->data[i];//getchar();
-                if (next_char <= 0)
-                        break;
-                if (!JSON_checker_char(jc, next_char))
-                {
-                        UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "SV_PubSub.c : JSON_checker_char(): syntax error");
-                        return;
-                }
-        }
-     }
-UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,"After new_JSON_checker \n");
-
-     if (jc)
-     {
-        if (!JSON_checker_done(jc))
-        {
-                UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "SV_PubSub.c : JSON_checker_done(): syntax error");
-                return;
-        }
-     }
-     UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "SV_PubSub.c : callback : JSON_Checker() success");
-     UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "SV_PubSub.c : callback : Moving to section 2 : json_tokener");
-
-
-
-     int stringlen = strlen((char*)encodedBuffer->data);
-     UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "SV_PubSub here 0 : encodedBuffer->data size is %d ", stringlen);
-     stringlen++ ;
-
-        json_char* json;
-        json_value* value;
-
-     if (stringlen > 0)      // jacky added 'if statement' on 3 Apr 2021
-     {
-        // convert UA_ByteString <encodedBuffer->data> to char*
-        char* myCharString = calloc(stringlen+1, sizeof(char));
-        strncpy(myCharString, (char*)encodedBuffer->data, stringlen-1);
-        myCharString[stringlen]='\0';
-
-        json = (json_char*)myCharString;
-        value = json_parse(json,stringlen);
-        if (value == NULL) {
-                UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "SV_PubSub.c : Unable to parse data");
-                free(myCharString);
-                return;
-        }
-
-        process_value(value, 0);
-
-        json_value_free(value);
-        free(myCharString);
-        return;
-    }
-}
-#endif
-
